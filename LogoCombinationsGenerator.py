ab3d6896fadbaa92ece343ae11aa7abbf60030b2
José Luis Barrera-Canto <jose.luis.barrera.canto@gmail.com>
2024-04-26 12:15:29 -0600

    Added feature to convert images to monochrome scale.

--- LogoCombinationsGenerator.py
+++ LogoCombinationsGenerator.py
@@ -1,12 +1,13 @@
 import cv2
 import numpy as np
 
+from PIL import Image
 from itertools import chain, combinations
 
 """Generator of combinations.
 
-    This program generates all possible combinations of sizes 2 to n, from a set of n political parties logos,
-    creating images for each and every combination.
+    This program generates all possible combinations of sizes 1 to n, from a set of n political parties logos,
+    creating images for each and every combination. If selected, the images are generated in grayscale.
     
 """
 
@@ -22,7 +23,7 @@ def combinator(
     Returns:
         An object containing all possible combinations.
     """
-    return chain.from_iterable(combinations(logos, r) for r in range(2, len(logos) + 1))
+    return chain.from_iterable(combinations(logos, r) for r in range(1, len(logos) + 1))
 
 
 def adjust_height(
@@ -40,16 +41,20 @@ def adjust_height(
     """
     hh, ww = image.shape[1::-1]
     width = int(height * ww / hh)
+
     return cv2.resize(image, (height, width))
 
 
 def create_combination_logo(
-    logos: tuple
+    logos: tuple,
+    grayscale: bool
 ) -> str:
-    """ Resizes an image to the specific height preserving the aspect ratio.
+    """ Resizes an image to the specific height preserving the aspect ratio,
+        and optionally converting to grayscale image.
 
     Args:
         logos: Tuple with data of images to combine.
+        grayscale: Boolean flag to generate grayscale instead of color images.
 
     Returns:
         Filename of the created image.
@@ -58,6 +63,11 @@ def create_combination_logo(
     filename = f'generated\\{"-".join([logo["name"] for logo in logos])}.png'
     combination_logo = np.hstack(images)
     cv2.imwrite(filename, combination_logo)
+    # Convert to grayscale if requested
+    if grayscale:
+        gray_image = Image.open(filename).convert('LA')
+        gray_image.save(filename)
+
     return filename
 
 
@@ -67,17 +77,17 @@ if __name__ == '__main__':
     # political party, and the path for the image file containing the logo. IMPORTANT: Tuple's order will define
     # the combining order when creating the new images.
     logos_base = (
-        {'name': 'PP1', 'filename': 'base\\pp1.png'},
-        {'name': 'PP2', 'filename': 'base\\pp2.png'},
-        {'name': 'PP3', 'filename': 'base\\pp3.png'},
-        {'name': 'PP4', 'filename': 'base\\pp4.png'},
-        {'name': 'PP5', 'filename': 'base\\pp5.png'},
-        {'name': 'PP6', 'filename': 'base\\pp6.png'},
-        {'name': 'PP7', 'filename': 'base\\pp7.png'},
-        {'name': 'PP8', 'filename': 'base\\pp8.png'}
+        {'name': 'P1', 'filename': 'base\\p1.png'},
+        {'name': 'P2', 'filename': 'base\\p2.png'},
+        {'name': 'P3', 'filename': 'base\\p3.png'},
+        {'name': 'P4', 'filename': 'base\\p4.png'},
+        {'name': 'P5', 'filename': 'base\\p5.png'},
+        {'name': 'P6', 'filename': 'base\\p6.png'},
+        {'name': 'P7', 'filename': 'base\\p7.png'},
+        {'name': 'P8', 'filename': 'base\\p8.png'}
     )
 
     for combination in combinator(logos_base):
-        print(create_combination_logo(combination), ' ... DONE!')
+        print(create_combination_logo(combination, True), ' ... DONE!')
 
     print('\nALL DONE!!')
